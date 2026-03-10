6d59a2619348430b7038b2e2cf8a3cc0955904b0
Siyuan Shen <siyuan.shen@inf.ethz.ch>
2026-03-10 16:36:03 +0100

    Modified script for building vllm on Alps

--- apps/ai/scripts/build_vllm.sh
+++ apps/ai/scripts/build_vllm.sh
@@ -8,16 +8,19 @@
 #SBATCH --mem=400GB
 #SBATCH --time=02:00:00
 
-export VLLM_DIR=/capstor/scratch/cscs/sshen/workspace/spcl-atlahs/apps/ai/vllm
-export MAX_JOBS=24
+export VLLM_DIR=/capstor/scratch/cscs/sshen/workspace/atlahs/apps/ai/vllm
+export CMAKE_GENERATOR="Ninja"
+export MAX_JOBS=16
+export CMAKE_BUILD_PARALLEL_LEVEL=16
 export VERBOSE=1
-export TORCH_CUDA_ARCH_LIST=90
-export CUDAARCHS=90
-export FLASH_ATTENTION_CUDA_ARCH_LIST=90
-export TMPDIR=/capstor/scratch/cscs/sshen/workspace/spcl-atlahs/apps/ai/vllm/.tmp
-export CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=90 -DFA2_ARCHS=90 -DFA3_ARCHS=90"
-
-srun --environment=vllm2 --export=ALL,VLLM_DIR,MAX_JOBS,CMAKE_ARGS,VERBOSE,TMPDIR bash -c "
+export TORCH_CUDA_ARCH_LIST="9.0"
+export CUDAARCHS="90"
+export FLASH_ATTENTION_CUDA_ARCH_LIST="90"
+export TMPDIR=/capstor/scratch/cscs/sshen/workspace/atlahs/apps/ai/vllm/.tmp
+export CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=90 -DFA2_ARCHS=9.0 -DFA3_ARCHS=9.0"
+export NVCC_FLAGS="-gencode arch=compute_90,code=sm_90"
+export VLLM_PREFIX=${VLLM_DIR}/install
+srun --environment=vllm --export=ALL,VLLM_DIR,MAX_JOBS,CMAKE_ARGS,VERBOSE,TMPDIR,NVCC_FLAGS,VLLM_PREFIX bash -c "
 cd ${VLLM_DIR} &&
-pip install -e . --no-deps --no-build-isolation -v
+pip install --prefix "${VLLM_PREFIX}" -e . --no-deps --no-build-isolation -v
 "
